78aa46cdcd7896a03cbda53577fba91f436c8ad1
liao1995 <346950981@qq.com>
2015-06-13 23:23:44 +0800

    using popen_noshell (clone and exec) replace popen to make faster.

--- login.c
+++ login.c
@@ -5,11 +5,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "popen_noshell.h"
 
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE);} while (0)
+                               
 #define MAX_LEN 256
 #define BUFF_LEN 1024
 
-int main()
+int main(int argc, char **argv)
 {
     char username[MAX_LEN], ip[MAX_LEN], password[MAX_LEN];
     char buff[BUFF_LEN];
@@ -37,34 +40,26 @@ int main()
 		  "interact\n", username, ip, password);
     script_fd = open("login.sh", O_WRONLY|O_CREAT, S_IRWXU|S_IXGRP);
     if (write(script_fd, buff, strlen(buff)) < 0)
-    {
-      perror("write");
-      exit(1);
-    }
+      errExit("write");
     if (close(script_fd))
-    {
-      perror("close");
-      exit(1);
-    }
+      errExit("close");
     
     // execute script
-    pin = popen("./login.sh", "w");
+    struct popen_noshell_pass_to_pclose pclose_arg;
+    pin = popen_noshell_compat("./login.sh", "w", &pclose_arg);
     if(!pin)
-    {
-      perror("popen");
-      exit(1);
-    }
+      errExit("popen_noshell_compat");
     
     while(1)
     {
       // read command from standard input
       fgets(buff, BUFF_LEN, stdin);
-      
+
       // write to shell input
       fputs(buff, pin);
       fflush(pin);
     }
-    pclose(pin);
+    pclose_noshell(&pclose_arg);
     printf("Bye\n");
     return 0;
 }
