53d08471d98ea9e9cb4ed3790730555284969b2f
spff <spff@bitmessage.ch>
2015-06-21 00:44:51 +0800

    merge the x function

--- ConferenceServer/main.cpp
+++ ConferenceServer/main.cpp
@@ -18,7 +18,13 @@
 #include <mutex>
 
 #include <X11/Xlib.h>
-
+#include <X11/Xutil.h>
+#include <X11/Xresource.h>
+#include <X11/extensions/XTest.h>
+#include <X11/extensions/XInput2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern "C" {
     #include "popen_noshell.h"
@@ -46,6 +52,12 @@ class OneConnection{
         int sockfd;
         string cmd, ip, usrname;
 
+
+        Display *dpy;
+        Window window;
+
+
+
     public:
 
         OneConnection(int sockfd){
@@ -60,12 +72,13 @@ class OneConnection{
                 return;
 
             SendResolution();
-
+            GeneratePointer();
             thread t1{&OneConnection::PopOutTerminal, this};
             thread t2{&OneConnection::GetSignalTillEnd, this};
 
             t1.join();
             t2.join();
+            RemovePointer();
             close(sockfd);
         }
 
@@ -114,6 +127,17 @@ class OneConnection{
             write(sockfd, fakereso.c_str(), fakereso.length());
         }
 
+        void GeneratePointer(){
+
+            //run "xinput -create-master \"" + ip + usrname + "\"";
+            //find id by get the output of "xinput" and find the ip + usrname + " Pointer", and the next will be its id
+        }
+
+        void RemovePointer(){
+
+            //run "xinput -remove-master \"" + ip + usrname + "\"";
+        }
+
         int PopOutTerminal(){
             FILE *pin;
             struct popen_noshell_pass_to_pclose pclose_arg;
@@ -133,6 +157,8 @@ class OneConnection{
 
         void GetSignalTillEnd(){
 
+            init();
+
             while(true){
                 int nbytes;
                 if((nbytes = recv(sockfd, recvline, RCVBUFSIZE, 0)) <= 0){
@@ -142,14 +168,118 @@ class OneConnection{
                 }
                 cout << recvline << endl;
 
+
                 /*if(isMouse){
                     MouseAct();
                 }
                 else if(iskeyboard){
                     KeyboardAct();
                 }*/
+
             }
+
         }
+
+
+        Bool init(){
+          // connect to X server
+          // arg: NULL, it defaults to the value of the DISPLAY environment variable.
+          if ((dpy=XOpenDisplay(NULL)) == NULL){
+            fprintf(stderr, "fail to open display!\n");
+            return False;
+          }
+
+          //returns the root window for the default screen.
+          window = DefaultRootWindow(dpy);
+
+          return True;
+        }
+
+
+
+        /**
+         * @param which: which button
+         * 	1 - "left",
+         * 	2 - "middle",
+         * 	3 - "right",
+         * 	4 - "fourth",
+         * 	5 - "fifth"
+         * @param what: what event is
+         * 	ButtonPress
+         * 	ButtonRelease
+         */
+        Bool GenerateMouseEvent(int which, int what){
+          return XTestFakeButtonEvent(dpy, which, what==ButtonPress, CurrentTime);
+        }
+
+        /*****************************************************************************
+        Bool GenerateMouseEvent(int which, int what)
+        {
+          XEvent event;
+          memset(&event, 0x00, sizeof(event));
+
+          event.type 			= what;
+          event.xbutton.button 		= which;
+          // the event window is on the same screen as the root window
+          event.xbutton.same_screen 	= True;
+
+          //get pointer coordinates - assign to event
+          XQueryPointer(dpy,
+                RootWindow(dpy, DefaultScreen(dpy)),
+                &event.xbutton.root,
+                &event.xbutton.window,
+                &event.xbutton.x_root,
+                &event.xbutton.y_root,
+                &event.xbutton.x,
+                &event.xbutton.y,
+                &event.xbutton.state	// state: button or key mask
+                  );
+          event.xbutton.subwindow = event.xbutton.window; // child window
+
+          while (event.xbutton.subwindow)
+          {
+            event.xbutton.window = event.xbutton.subwindow;
+            XQueryPointer(dpy,
+                  event.xbutton.window,
+                  &event.xbutton.root,
+                  &event.xbutton.subwindow,
+                  &event.xbutton.x_root,
+                  &event.xbutton.y_root,
+                  &event.xbutton.x,
+                  &event.xbutton.y,
+                  &event.xbutton.state
+            );
+          }
+          int rc = XSendEvent(dpy,
+                      PointerWindow, // he destination window is the window that contains the pointer.
+                      True, 	     // propagate
+                      0xfff,	     // event mask
+                      &event
+                    );
+
+          XFlush(dpy);
+
+          return rc;
+        }
+        ******************************************************************************/
+
+        /* New */
+        void MoveMousePointer(int deviceid, int x, int y){
+          XIWarpPointer(dpy, deviceid, None, window, 0, 0, 0, 0, x, y);
+          XFlush(dpy);
+        }
+
+        /**
+         * @param which: which key
+         * @param what: what event is
+         * 	KeyPress
+         * 	KeyRelease
+         */
+        Bool GenerateKeyEvent(unsigned int which, int what){
+          return XTestFakeKeyEvent(dpy, which, what==KeyPress, CurrentTime);
+        }
+
+
 
 };
 
